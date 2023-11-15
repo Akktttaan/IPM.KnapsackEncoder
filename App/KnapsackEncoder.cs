18d231d05570b77d9ca152b95279dcdc4ffbd5e4
Aktan Astarkulov <astarkulov.aktan@gmail.com>
2023-11-15 10:12:38 +0600

    Добавить файлы проекта

---
+++ App/KnapsackEncoder.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using MathNet.Numerics;
+
+namespace App
+{
+    public class KnapsackEncoder
+    {
+        private readonly int _keyDimension;
+        private readonly string _text;
+
+        public KnapsackEncoder(int keyDimension, string text)
+        {
+            _keyDimension = keyDimension;
+            _text = text;
+        }
+
+        private int[] GenerateSuperincreasingSequence()
+        {
+            var rnd = new Random();
+            var sequence = new int[_keyDimension];
+            sequence[0] = rnd.Next(0, 10);
+
+            for (var i = 1; i < _keyDimension; i++)
+            {
+                sequence[i] = rnd.Next(sequence.Sum() * 2 + 1, sequence.Sum() * 2 + rnd.Next(2, 20));
+            }
+
+            return sequence;
+        }
+
+        public (int[], int, int) Encrypt()
+        {
+            var rnd = new Random();
+            var privateKey = GenerateSuperincreasingSequence();
+            var m = rnd.Next(privateKey.Sum() + 1,
+                privateKey.Sum() + 100);
+            var n = Constants.PrimeNumbers.First();
+            var i = 1;
+            while (Euclid.GreatestCommonDivisor(n, m) != 1)
+            {
+                n = Constants.PrimeNumbers[i];
+                i++;
+            }
+
+            var openKey = privateKey.Select(x => x * n % m).ToArray();
+
+            var binaryText = ConvertTextToBinary(_text);
+            var encryptedValues = new int[binaryText.Length / _keyDimension + 1];
+            for (i = 0; i < binaryText.Length / _keyDimension; i++)
+            {
+                var startIndex = i * _keyDimension;
+                var remainingLength = Math.Min(_keyDimension, binaryText.Length - startIndex);
+                var chunk = binaryText.Substring(startIndex, remainingLength);
+                if (chunk.Length < _keyDimension) chunk.PadLeft(_keyDimension, '0');
+                encryptedValues[i] = SumNumbersByBits(openKey, chunk);
+            }
+
+            return (encryptedValues, n, m);
+        }
+
+        public string Decrypt(int[] encryptedValues, int n, int m)
+        {
+            var inverseN = ModInverse(n, m);
+            var binariyes = encryptedValues.Select(x => x * inverseN % m).ToArray();
+
+            return ConvertBinaryToText(binariyes);
+        }
+
+        private string ConvertTextToBinary(string text)
+        {
+            var binaryStringBuilder = new StringBuilder();
+
+            foreach (var binary in text.Select(c => Convert.ToString(Constants.RussianLetters[c.ToString()], 2)))
+            {
+                binaryStringBuilder.Append(binary);
+            }
+
+            return binaryStringBuilder.ToString();
+        }
+
+        public string ConvertBinaryToText(int[] binaryText)
+        {
+            var textBuilder = new StringBuilder();
+
+            for (var i = 0; i < binaryText.Length; i += 8)
+            {
+                textBuilder.Append(Constants.RussianLettersReverse[binaryText[i]]); // Добавление символа в текст
+            }
+
+            return textBuilder.ToString();
+        }
+     
+
+        public int SumNumbersByBits(int[] numbers, string bits)
+        {
+            if (bits.Length != numbers.Length)
+            {
+                throw new ArgumentException("Длина массива битов должна быть равна длине массива чисел.");
+            }
+
+            var sum = 0;
+            for (var i = 0; i < bits.Length; i++)
+            {
+                if (bits[i] == '1')
+                {
+                    sum += numbers[i];
+                }
+            }
+
+            return sum;
+        }
+
+        // Метод для вычисления модульного обратного
+        public int ModInverse(int n, int m)
+        {
+            int a = n, b = m;
+            int x = 0, y = 1, lastX = 1, lastY = 0;
+            int temp;
+
+            while (b != 0)
+            {
+                var quotient = a / b;
+                var remainder = a % b;
+
+                a = b;
+                b = remainder;
+
+                temp = x;
+                x = lastX - quotient * x;
+                lastX = temp;
+
+                temp = y;
+                y = lastY - quotient * y;
+                lastY = temp;
+            }
+
+            if (lastX < 0)
+            {
+                lastX += m;
+            }
+
+            return lastX;
+        }
+    }
+}
---
+++ Client/MainWindow.xaml.cs
@@ -0,0 +1,137 @@
+﻿using App;
+using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int n, m;
+        private int[] encryptedValues;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            KeyDimension.ItemsSource = Enumerable.Range(1, 150);
+        }
+
+        private void GenerationButton_Click(object sender, RoutedEventArgs e)
+        {
+            var rnd = new Random();
+            KeyDimension.SelectedItem = rnd.Next(1, 150);
+        }
+
+        private void EncryptButton_Click(object sender, RoutedEventArgs e)
+        {
+            var encoder = new KnapsackEncoder((int)KeyDimension.SelectedItem, InitialText.Text);
+            var encrypted = encoder.Encrypt();
+            encryptedValues = encrypted.Item1;
+            n = encrypted.Item2;
+            m = encrypted.Item3; 
+            ConvertedText.Text = string.Join(" ", encryptedValues);
+        }
+        private void DecryptButton_Click(object sender, RoutedEventArgs e)
+        {
+            var encoder = new KnapsackEncoder((int)KeyDimension.SelectedItem, InitialText.Text);
+            InitialText.Text = ConvertedText.Text;
+            ConvertedText.Text = encoder.Decrypt(encryptedValues, n, m);
+        }
+
+        private void LoadTextFromFile(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*"
+            };
+
+            if (openFileDialog.ShowDialog() != true) return;
+            try
+            {
+                var filePath = openFileDialog.FileName;
+
+                var encoding = DetectFileEncoding(filePath);
+
+                var fileContent = File.ReadAllText(filePath, encoding);
+
+                InitialText.Text = fileContent;
+                ConvertedText.Text = string.Empty;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Произошла ошибка при загрузке файла: {ex.Message}");
+            }
+        }
+
+        private Encoding DetectFileEncoding(string filePath)
+        {
+            // Определите кодировку файла на основе его содержимого
+            // Можно использовать различные методы, например, на основе магических байтов файла
+
+            var buffer = new byte[4];
+            using (var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                fileStream.Read(buffer, 0, 4);
+            }
+
+            return buffer[0] switch
+            {
+                0xef when buffer[1] == 0xbb && buffer[2] == 0xbf => Encoding.UTF8,
+                0xff when buffer[1] == 0xfe => Encoding.Unicode,
+                0xfe when buffer[1] == 0xff => Encoding.BigEndianUnicode,
+                _ => Encoding.Default
+            };
+        }
+
+        private void SaveTextInFile(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Получаем текст из TextBox
+                string textToSave = ConvertedText.Text;
+
+                // Открываем диалоговое окно для выбора места сохранения файла
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+
+                if (saveFileDialog.ShowDialog() == true)
+                {
+                    // Получаем путь к выбранному файлу
+                    string filePath = saveFileDialog.FileName;
+
+                    // Сохраняем текст в файл
+                    File.WriteAllText(filePath, textToSave);
+
+                    MessageBox.Show("Текст успешно сохранен в файл.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Произошла ошибка при сохранении файла: {ex.Message}");
+            }
+        }
+
+        private void KeyDimensionChanged(object sender, RoutedEventArgs e)
+        {
+            ValidateEncyptDecryptButton();
+        }
+
+        private void InitialTextChanged(object sender, RoutedEventArgs e)
+        {
+            ValidateEncyptDecryptButton();
+        }
+
+        private void ValidateEncyptDecryptButton()
+        {
+            var state = KeyDimension.SelectedItem is not null && !string.IsNullOrEmpty(InitialText.Text);
+            EncryptButton.IsEnabled = state;
+            DecryptButton.IsEnabled = state;
+        }
+    }
+}
