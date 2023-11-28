4527b23e0d12ea25d3718891976e124b4e784bf5
Aktan Astarkulov <astarkulov.aktan@gmail.com>
2023-11-28 19:42:29 +0600

    feat: some features

---
+++ App/BigIntegerExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace App
+{
+    public static class BigIntegerExtensions
+    {
+        public static BigInteger Sum(this IEnumerable<BigInteger> arr)
+        {
+            return arr.Aggregate<BigInteger, BigInteger>(0, (current, item) => current + item);
+        } 
+    }
+}
---
+++ App/StringExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace App
+{
+    public static class StringExtensions
+    {
+        public static string ToBinary(this string str)
+        {
+            byte[] utf8Bytes = Encoding.UTF8.GetBytes(str);
+            return string.Join("", utf8Bytes.Select(b => Convert.ToString(b, 2).PadLeft(8, '0')));
+        }
+
+        public static string BinaryToString(this string binary)
+        {
+            List<byte> bytes = new List<byte>();
+
+            for (int i = 0; i < binary.Length; i += 8)
+            {
+                int endIndex = Math.Min(i + 8, binary.Length);
+                string byteString = binary.Substring(i, endIndex - i).PadRight(8, '0');
+                byte b = Convert.ToByte(byteString, 2);
+                bytes.Add(b);
+            }
+
+            return Encoding.UTF8.GetString(bytes.ToArray());
+        }
+    }
+}
